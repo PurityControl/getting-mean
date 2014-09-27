ae69cb21e5144c52cc1e73f75b18125bcc58f58d
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-27 22:45:02 +0100

    added mongodb as requirement for provisioning script

--- vagrant-trusty64-mean.sh
+++ vagrant-trusty64-mean.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install -y nodejs nodejs-legacy  npm git tree mc emacs24-nox
+sudo apt-get install -y nodejs nodejs-legacy  npm git tree mc emacs24-nox mongodb
 sudo npm install -g nodemon
