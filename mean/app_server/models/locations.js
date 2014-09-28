abcbc7116571ff3be6c61719b5f91ac3fdde9db4
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-28 07:27:58 +0000

    added opening time schema for location

--- mean/app_server/models/locations.js
+++ mean/app_server/models/locations.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose');
 
+var openingTimeSchema = mongoose.Schema ({
+  days: {type: String, required: true},
+  opening: String,
+  closing: String,
+  closed: {type: Boolean, required: true}
+});
+
 var locationSchema = mongoose.Schema ({
   name: {type: String, required: true},
   address: String,
   rating: {type: Number, "default": 0, min: 0, max: 5},
   facilities: [String],
   coords: {type: [Number], index: '2dsphere'}
+
 });
 
